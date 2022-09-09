fa64c48ae5bd994dbbbbdf8c735118b452f50ad3
Tornikekhizaneishvili <khizaneishvilitornike@gmail.com>
2022-09-09 22:41:27 +0400

    v1.01

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
-// let billValue = document.querySelector(".bill-value")
+const billAmount = document.getElementById('bill-amount')
+const tipAmount = document.querySelector('.select-tip')
 
-// console.log(billValue);
+
+
+
+console.log(tipAmount);
+
+billAmount.addEventListener('input' , print)
+tipAmount.addEventListener('click' , e)
+
+function e() {
+    console.log(tipAmount.value);
+}
